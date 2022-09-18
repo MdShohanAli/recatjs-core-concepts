e70ad38495422b2eb7de604da1ced3470cdfe011
MdShohanAli <720shohan@gmail.com>
2022-09-18 13:27:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 111;
+const singers = [
+  { name: 'mahafuzur', job: 'singer' },
+  { name: 'Eva Rohaman', job: 'singra' }
+]
+const singerStyle = {
+  color: 'red',
+  backgroundColor: 'white'
+}
+
+
 function App() {
+  const nayoks = ['Amir', 'bappraz', 'kuber', 'alu'];
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        nayoks.map(nayok => <li>name:  {nayok}</li>)
+      }
+      {
+        nayoks.map(nayok => <Person name={nayok} ></Person>)
+      }
+      {
+        singers.map(singer => <Player name={singer.name}>  </Player>)
+      }
+
+      {/* <Person name={nayoks[0]} nayka="musumi" ></Person>
+      <Person name={nayoks[1]} nayka="shekha" ></Person> */}
+      <h2>I am single</h2>
+      <h5>What happend</h5>
+
+
+      <Player> </Player>
     </div>
   );
 }
+function Person(props) {
+
+  return (
+    <div className='person' >
+      <h1>{props.name}</h1>
+      <p>Nayoka</p>
+    </div>
+  )
+}
+
+function Friend() {
+  return (
+    <div className='friend' >
+      <h3> al kayda </h3>
+      <p> yill baba </p>
+    </div>
+  )
+}
+
+function Player(props) {
+  return (
+    <div className='player'>
+      <h3></h3>
+    </div>
+  )
+
+}
+
+
 
 export default App;
